e8e7830ac7e86957bf11a6bd217589da879166c5
yantong <yantong@winning.com.cn>
2022-09-09 15:12:26 +0800

    样式修改

--- src/components/scale.vue
+++ src/components/scale.vue
@@ -61,8 +61,9 @@ export default {
 
       let scale = this.$refs.scale;
       let ctx = scale.getContext("2d");
+      let dpr = window.devicePixelRatio;
       let r = this.width / 2;
-      let center = [r, r + 40];
+      let center = [r, r + 20 * dpr];
       let realAngle = this.drawQueue.shift();
 
       scale.width = this.width;
@@ -70,23 +71,24 @@ export default {
       ctx.restore();
       ctx.translate(...center);
       ctx.rotate(Math.PI);
-      for (let i = 1; i <= 180; i++) {
+      for (let i = 0; i <= 180; i++) {
         ctx.beginPath();
-        ctx.strokeStyle = i % 10 == 0 ? "#00f" : "#00f";
-        ctx.strokeStyle = i >= 80 && i <= 100 ? "#0f0" : ctx.strokeStyle;
-        ctx.lineWidth = 1;
-        i > 1 && ctx.rotate(Math.PI / 180);
+        ctx.strokeStyle = i % 10 == 0 ? "#2D5AFA" : "#2D5AFA";
+        ctx.strokeStyle = i >= 80 && i <= 100 ? "#00AB44" : ctx.strokeStyle;
+        ctx.lineWidth = i % 10 == 0 ? 2 * dpr : 1 * dpr;
+        ctx.lineCap = "round";
+        i > 0 && ctx.rotate(Math.PI / 180);
         ctx.moveTo(r - 10, 0);
-        ctx.lineTo(r - (i % 10 == 0 ? 30 : 20), 0);
-        if (i >= 40 && i <= 140) ctx.stroke();
+        ctx.lineTo(r - (i % 10 == 0 ? 20 * dpr : 15 * dpr), 0);
+        if (i >= 40 && i <= 140 && i % 2 == 0) ctx.stroke();
       }
 
-      ctx.restore();
       ctx.beginPath();
-      ctx.strokeStyle = "#f00";
-      ctx.lineWidth = 1;
+      ctx.strokeStyle = "#EC0000";
+      ctx.lineWidth = 4 * dpr;
+      ctx.lineCap = "round";
       ctx.rotate(Math.PI + realAngle);
-      ctx.moveTo(r - 10, 0);
+      ctx.moveTo(r - 30 * dpr, 0);
       ctx.lineTo(0, 0);
       ctx.stroke();
 
@@ -94,8 +96,13 @@ export default {
     },
   },
   mounted() {
-    this.width = this.$refs.scale.offsetWidth;
-    this.height = this.$refs.scale.offsetHeight;
+    let ctx = this.$refs.scale.getContext("2d");
+    let dpr = window.devicePixelRatio;
+    let { width: cssWidth, height: cssHeight } =
+      this.$refs.scale.getBoundingClientRect();
+    this.width = dpr * cssWidth;
+    this.height = dpr * cssHeight;
+    ctx.scale(dpr, dpr);
 
     this.$nextTick(() => {
       this.makeDrawQueue();
@@ -108,6 +115,14 @@ export default {
 .scale {
   width: 100%;
   max-width: 600px;
-  height: 200px;
+  height: 140px;
+  background: #e9e9e9;
+  border-radius: 4px;
+}
+
+@media screen and (min-device-width: 600px) {
+  .scale {
+    height: 200px;
+  }
 }
 </style>
